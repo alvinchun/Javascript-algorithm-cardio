8d1ad9744de55e9b2d343eb85c666ddd055645a2
alvinchun992 <dev.alvinchun@gmail.com>
2022-02-03 16:17:19 +0900

    more recursion problems

--- recursion-problems.js
+++ recursion-problems.js
@@ -29,3 +29,69 @@ function factorial(n){
 }
 
 factorial(0)
+
+//3. productOfArray
+
+const productOfArray = (arr) => {
+  if (arr === undefined || null) {
+    return "put an array with numbers in it"
+  }
+
+   if (arr.length === 0) {
+    return 1;
+  }
+
+  return arr[0] * productOfArray(arr.slice(1))
+}
+
+console.log(productOfArray([1, 2, 3, 4, 5]));
+
+
+//4. recursiveRange ( adding all the numbers in array)
+
+// SAMPLE INPUT/OUTPUT
+// recursiveRange(6) // 21
+// recursiveRange(10) // 55 
+
+function recursiveRange(arr){
+   
+  let totalNum = 0;
+
+  if(arr.length === 0){
+      return 0;
+  }
+  
+  totalNum = arr[0] + recursiveRange(arr.slice(1))
+  return totalNum
+}
+
+recursiveRange([1,2,3,10])
+
+//5. fibonacci (n th numer is) => ?
+function fibonacci(num) {
+  if (num <= 2) return 1;
+  console.log(fibonacci(num - 1)) // 5 = 3 + 2 = 1 + 1
+//   console.log(fibonacci(num - 2)) // 3
+
+  return fibonacci(num - 1) + fibonacci(num - 2); 
+  // 
+}
+
+
+fibonacci(5) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
