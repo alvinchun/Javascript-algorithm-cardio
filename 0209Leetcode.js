f79448908b892f9df6595896916059978c02ce57
Alvin <dev.alvinchun@gmail.com>
2022-02-15 03:25:04 +0900

    solutions added

--- 0209Leetcode.js
+++ 0209Leetcode.js
@@ -1,14 +1,56 @@
-var removeDuplicates = function (nums) {
-  
-
-
-  let arrToSet = new Set(nums)
-  let setToArr = [...arrToSet]
-  return setToArr
-
+var maxProfit = function(prices) {
+    for(let i = 0; i < prices.length ; i++){
+      for(let j = 1; j < prices.length; i++){
+        if(prices[i] < prices[j]){
+          prices[j] - price
+        }
+      }
+    }
 };
 
 
-console.log(removeDuplicates([1,2,2,2,3,4]));
-// already sorted
-// 
+maxProfit([7,1,5,3,6,4])
+maxProfit([7,6,4,3,1])
+
+
+// Solution 1
+
+var maxProfit = function(prices) {
+  var min = Number.MAX_SAFE_INTEGER; 
+  var max = 0;
+  for (var i = 0; i < prices.length; i++) {
+      min = Math.min(min, prices[i]);
+      max = Math.max(max, prices[i] - min);
+  }
+  return max;
+};
+
+// Solution 2
+
+var maxProfit = function(prices) {
+  let min = prices[0], max = 0;
+  
+  for(let i = 1; i < prices.length; i++){
+      if(prices[i] - min > max){
+          max = prices[i] - min;
+      }
+      
+      if(prices[i] < min) min = prices[i]
+  }
+  
+  return max;
+};
+
+// Solution 3
+
+var maxProfit = function(prices) {
+  let maxProf = 0, curr = prices[0];
+  
+  for(let x of prices) {
+      if(curr < x) maxProf = Math.max(maxProf, x - curr);
+      else curr = x;
+  }
+  return maxProf;
+};
+
+
